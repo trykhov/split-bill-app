e503fdf4cbfe6daf20a70b9d91a153d155ce8f86
Try <khovct@gmail.com>
2019-06-20 13:14:25 -0700

    added bcrypt instead of md5

--- back-end/app.js
+++ back-end/app.js
@@ -1,40 +1,70 @@
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const keys = require('./keys');
-const md5 = require('md5');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
+mongoose.connect(
+  process.env.DB_URL,
+  { useNewUrlParser: true }
+);
 
 const loginSchema = {
   username: String,
   password: String
 };
 
-const User = new mongoose.model('login', loginSchema);
+const User = new mongoose.model("login", loginSchema);
 
-const newUser = new User({
-  username: "testing",
-  password: md5("password-test")
+bcrypt.hash("wrong", saltRounds, function(err, hash) {
+  const newUser = new User({
+    username: "testing2",
+    password: hash
+  });
+
+  // newUser.save(function(err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Success, check the database");
+  //   }
+  // });
 });
 
 // testing purposes
-User.findOne({username: "testing", password: md5("hi")}, function(err, foundUser) {
-  if(err) {
+User.findOne({ username: "testing2" }, function(err, foundUser) {
+  if (err) {
     console.log(err);
-  } else if(!foundUser){
+  } else if (!foundUser) {
     console.log("can't find anyone");
   } else {
-    console.log("Logged in!");
+    bcrypt.compare("wrong", foundUser.password, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else if (result == true) {
+        console.log(foundUser.password);
+      } else {
+        console.log("Nah");
+      }
+    });
   }
-})
+});
+
+// testing purposes
+// User.findOne({username: "testing"}, function(err, foundUser) {
+//   if(err) {
+//     console.log(err);
+//   } else if(!foundUser){
+//     console.log("can't find anyone");
+//   } else if(foundUser){
+//     bcrypt.compare()
+//   }
+// })
 
 // newUser.save(function(err) {
 //   if(err) {
@@ -44,8 +74,6 @@ User.findOne({username: "testing", password: md5("hi")}, function(err, foundUser
 //   }
 // });
 
-
-
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
-})
+});
