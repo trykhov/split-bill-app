b13830d20cbaee5190c0a8707896a25640a05013
Try <khovct@gmail.com>
2019-08-21 20:00:49 -0700

    customized the numPeople.js

--- front-end/src/components/NumPeople.js
+++ front-end/src/components/NumPeople.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import logo from '../logo.png';
+import '../css/numPeople.css';
+import { Link } from 'react-router-dom';
 
 class NumPeople extends React.Component {
     render() {
         return (
-            <div>NumPeople</div>
+            <div id="numPeoplePage">
+                <div className="appName">
+                    {/* this will be the container that holds the app logo and name at the top left corner */}
+                    <img className="appLogo" src={logo} alt="logo"/> 
+                    <label>SplitBill</label>
+                </div>
+                <div id="numBox">
+                    <label id="howMany">How Many People?</label>
+                    <input id="enterNum" />
+                </div>
+            </div>
         )
     }
 };
