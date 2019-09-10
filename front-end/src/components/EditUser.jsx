25c97e8d4ea3b90aca57565047a7344ba3db8f3c
Try <khovct@gmail.com>
2019-09-10 16:27:48 -0700

    added calculations that determines who pays based on the checks

--- front-end/src/components/EditUser.jsx
+++ front-end/src/components/EditUser.jsx
@@ -5,32 +5,83 @@ import { connect } from "react-redux";
 import "../css/editUser/editUser.css";
 
 class EditUser extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      numUnChecked: this.props.numPpl
+    };
   }
+
+  checkCheckBox = index => {
+    const { numUnChecked } = this.state;
+    if (document.getElementById(`hasPaid${index}`).checked) {
+      this.setState({ numUnChecked: numUnChecked - 1 });
+    } else {
+      this.setState({ numUnChecked: numUnChecked + 1 });
+    }
+  };
+
+  addSubtractDollar = (index, amount) => {
+    const { numUnChecked } = this.state;
+
+    if (!document.getElementById(`hasPaid${index}`).checked) {
+      // if the box is not checked, you can still add more to pay
+      const personAmount = document.getElementById(`person${index}`);
+      // changing the value of the input
+      personAmount.value = (Number(personAmount.value) + amount).toFixed(2);
+      // get all the payment boxes (people)
+      const peopleToPay = document.getElementsByClassName("hasToPay");
+      // const leftOverPay = peopleToPay.filter(check => check.checked === true);
+      // console.log(leftOverPay.length)
+      for (let i = 1; i <= peopleToPay.length; i += 1) {
+        const confirmCheckBox = document.getElementById(`hasPaid${i}`).checked;
+        const deduceTarget = document.getElementById(`person${i}`);
+        if (!confirmCheckBox && i !== index) {
+          deduceTarget.value = (
+            Number(deduceTarget.value) -
+            amount / (numUnChecked - 1)
+          ).toFixed(2);
+        }
+      }
+    }
+  };
 
   generatePersonList = numberOfPeople => {
     const dummyArr = new Array(numberOfPeople).fill(0);
+    const { wholeBill } = this.props;
     return dummyArr.map((list, index) => {
       return (
         // eslint-disable-next-line react/no-array-index-key
-        <div key={index} className="formComponentContainers">
+        <div id="peoplePaying" key={index} className="formComponentContainers">
           <div className="personContainer">
-            {/* check box */}
+            <input
+              type="checkbox"
+              id={`hasPaid${index + 1}`}
+              className="hasToPay"
+              onClick={() => this.checkCheckBox(index + 1)}
+            />
             Person {index + 1}
           </div>
           <div className="paymentContainer">
-            <button type="button" className="plusSign">
+            <button
+              type="button"
+              className="plusSign"
+              onClick={() => this.addSubtractDollar(index + 1, 1)}
+            >
               +
             </button>
             <input
               type="text"
               readOnly
               className="paymentValue"
-              defaultValue="$1222.45"
+              id={`person${index + 1}`}
+              defaultValue={(wholeBill / numberOfPeople).toFixed(2)}
             />
-            <button type="button" className="minusSign">
+            <button
+              type="button"
+              className="minusSign"
+              onClick={() => this.addSubtractDollar(index + 1, -1)}
+            >
               –
             </button>
           </div>
