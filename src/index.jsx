076d5725a2af83f3a0abe6e067e831d47d928f4c
Try <khovct@gmail.com>
2019-09-20 02:07:34 -0700

    added HashRouter and basename to allow app to run on github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
@@ -12,11 +12,12 @@ const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    {/* Use HashRouter basename="/" for it to run sucessfully on Github Pages */}
+    <HashRouter basename="/"> 
       <Route path="/" component={TitlePage} exact />
       <Route path="/billInfo" component={TotalBill} />
       <Route path="/modifyBill" component={EditUser} />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
